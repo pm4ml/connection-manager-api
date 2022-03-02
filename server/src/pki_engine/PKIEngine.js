58753747d87948c703b4a285516901c066aa660e
Yevhen Kyriukha <kirgene@gmail.com>
2022-03-02 20:46:59 +0200

    fix

--- server/src/pki_engine/PKIEngine.js
+++ server/src/pki_engine/PKIEngine.js
@@ -15,7 +15,7 @@
  *  limitations under the License.                                            *
  ******************************************************************************/
 
-const { getValidationConfig } = require('./this.validationConfig');
+const { getValidationConfig } = require('./ValidationsConfiguration');
 const ValidationCodes = require('./ValidationCodes');
 const Validation = require('./Validation');
 const forge = require('node-forge');
