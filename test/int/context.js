1873f7357a42e28ae2b4ac1dc58eb9c63652027a
geka.evk <geka.evk@gmail.com>
2023-10-10 17:20:51 +0100

    IPROD-189-3 Fixing test/int

--- test/int/context.js
+++ test/int/context.js
@@ -2,6 +2,7 @@ const VaultPKIEngine = require('../../src/pki_engine/VaultPKIEngine');
 const Constants = require('../../src/constants/Constants');
 
 exports.createContext = async () => {
+  console.log('!!!!!!!!!   VAULT CONFIG:   !!!!!!!!!', Constants.vault);
   const pkiEngine = new VaultPKIEngine(Constants.vault);
   await pkiEngine.connect();
   return {
