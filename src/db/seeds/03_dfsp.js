1ac9892d88d06ce9dd8e495ebf9da48b5e1837f4
Kalin Krustev <kalin.krustev@gmail.com>
2024-07-29 15:21:44 +0000

    fix: read config only if DFSP_SEED is set

--- src/db/seeds/03_dfsp.js
+++ src/db/seeds/03_dfsp.js
@@ -1,10 +1,9 @@
-const Constants = require('../../constants/Constants');
-const { createCSRAndDFSPOutboundEnrollment } = require('../../service/DfspOutboundService');
-const PKIEngine = require('../../pki_engine/VaultPKIEngine');
-
 // pass DFSP_SEED=DFSP1:KES,DFSP2:MWK,DFSP3:UGX to seed the dfsp with the given monetary zones
 exports.seed = async (knex) => {
     if (process.env.DFSP_SEED) {
+        const Constants = require('../../constants/Constants');
+        const { createCSRAndDFSPOutboundEnrollment } = require('../../service/DfspOutboundService');
+        const PKIEngine = require('../../pki_engine/VaultPKIEngine');
         const dfsps = process.env.DFSP_SEED.split(',')
             .map(dfsp => dfsp.split(':').map(s => s.trim()));
 
@@ -19,6 +18,10 @@ exports.seed = async (knex) => {
 
         const pkiEngine = new PKIEngine(Constants.vault);
         await pkiEngine.connect();
-        for (const [dfsp_id] of dfsps) await createCSRAndDFSPOutboundEnrollment({pkiEngine}, dfsp_id);
+        for (const [dfsp_id] of dfsps) await createCSRAndDFSPOutboundEnrollment(
+            {pkiEngine},
+            dfsp_id,
+            Constants.clientCsrParameters
+        );
     }
 };
