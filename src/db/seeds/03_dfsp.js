36ae22054255d36d1689d6775429f8995cfa54a7
Kalin Krustev <kalin.krustev@gmail.com>
2024-07-29 14:38:42 +0000

    fix: create CSR during DFSP seeding

--- src/db/seeds/03_dfsp.js
+++ src/db/seeds/03_dfsp.js
@@ -1,13 +1,24 @@
+const Constants = require('../../constants/Constants');
+const { createCSRAndDFSPOutboundEnrollment } = require('../../service/DfspOutboundService');
+const PKIEngine = require('../../pki_engine/VaultPKIEngine');
+
 // pass DFSP_SEED=DFSP1:KES,DFSP2:MWK,DFSP3:UGX to seed the dfsp with the given monetary zones
-exports.seed = (knex) =>
-    process.env.DFSP_SEED &&
-    knex('dfsps').insert(
-        process.env.DFSP_SEED.split(',')
-        .map(dfsp => dfsp.split(':').map(s => s.trim()))
-        .map(([dfsp_id, monetaryZoneId]) => ({
-            dfsp_id,
-            name: dfsp_id,
-            monetaryZoneId,
-            security_group: `Application/DFSP:${dfsp_id}`,
-        }))
-    ).onConflict('dfsp_id').merge();
+exports.seed = async (knex) => {
+    if (process.env.DFSP_SEED) {
+        const dfsps = process.env.DFSP_SEED.split(',')
+            .map(dfsp => dfsp.split(':').map(s => s.trim()));
+
+        await knex('dfsps').insert(
+            dfsps.map(([dfsp_id, monetaryZoneId]) => ({
+                dfsp_id,
+                name: dfsp_id,
+                monetaryZoneId,
+                security_group: `Application/DFSP:${dfsp_id}`,
+            }))
+        ).onConflict('dfsp_id').merge();
+
+        const pkiEngine = new PKIEngine(Constants.vault);
+        await pkiEngine.connect();
+        for (const [dfsp_id] of dfsps) await createCSRAndDFSPOutboundEnrollment({pkiEngine}, dfsp_id);
+    }
+};
