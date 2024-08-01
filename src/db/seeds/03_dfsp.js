3573cff1d9aae6ae1fa99eeb6b742b8c7306bd9a
Kalin Krustev <kalin.krustev@gmail.com>
2024-08-01 13:25:43 +0300

    fix: dfsp seed csr and isProxy (#83)

--- src/db/seeds/03_dfsp.js
+++ src/db/seeds/03_dfsp.js
@@ -1,13 +1,33 @@
 // pass DFSP_SEED=DFSP1:KES,DFSP2:MWK,DFSP3:UGX to seed the dfsp with the given monetary zones
-exports.seed = (knex) =>
-    process.env.DFSP_SEED &&
-    knex('dfsps').insert(
-        process.env.DFSP_SEED.split(',')
-        .map(dfsp => dfsp.split(':').map(s => s.trim()))
-        .map(([dfsp_id, monetaryZoneId]) => ({
-            dfsp_id,
-            name: dfsp_id,
-            monetaryZoneId,
-            security_group: `Application/DFSP:${dfsp_id}`,
-        }))
-    ).onConflict('dfsp_id').merge();
+exports.seed = async (knex) => {
+    if (process.env.DFSP_SEED) {
+        const Constants = require('../../constants/Constants');
+        const { createCSRAndDFSPOutboundEnrollment, getDFSPOutboundEnrollments } = require('../../service/DfspOutboundService');
+        const PKIEngine = require('../../pki_engine/VaultPKIEngine');
+        const dfsps = process.env.DFSP_SEED.split(',')
+            .map(dfsp => dfsp.split(':').map(s => s.trim()));
+
+        await knex('dfsps').insert(
+            dfsps.map(([dfsp_id, monetaryZoneId, isProxy]) => ({
+                dfsp_id,
+                name: dfsp_id,
+                monetaryZoneId: monetaryZoneId || null,
+                security_group: `Application/DFSP:${dfsp_id}`,
+                isProxy: isProxy === 'proxy'
+            }))
+        ).onConflict('dfsp_id').merge();
+
+        const pkiEngine = new PKIEngine(Constants.vault);
+        await pkiEngine.connect();
+        for (const [dfsp_id] of dfsps) {
+            const exists = await getDFSPOutboundEnrollments({pkiEngine}, dfsp_id);
+            if (exists.length === 0) {
+                await createCSRAndDFSPOutboundEnrollment(
+                    {pkiEngine},
+                    dfsp_id,
+                    Constants.clientCsrParameters
+                );
+            }
+        }
+    }
+};
