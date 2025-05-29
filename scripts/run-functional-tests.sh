e8680b2d4722a9c06bb8e03a2b97e35fce80a3d3
Kevin Leyow <kleyow@gmail.com>
2025-05-28 20:56:00 -0500

    chore: script

--- scripts/run-functional-tests.sh
+++ scripts/run-functional-tests.sh
@@ -1,35 +1,15 @@
-#!/usr/bin/env bash
+#!/bin/bash
+set -eo pipefail
 
-# {{{ Bash settings
-# abort on nonzero exitstatus
-  set -o errexit
-# don't hide errors within pipes
-  set -o pipefail
-# }}}
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+PROJECT_ROOT="$(cd "${SCRIPT_DIR}/../.." && pwd)"
 
-# {{{ Variables
-  # local
-  readonly script_name=$(basename "${0}")
-  readonly script_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+cd "${PROJECT_ROOT}"
 
-  # from exports
-  readonly GIT_URL=${GIT_URL:-"https://github.com/pm4ml/connection-manager-api.git"}
-  readonly GIT_TAG=${GIT_TAG:-"v2.10.3"}
-  readonly TARGET_DIR=${TARGET_DIR:-"/tmp/tests"}
-  IFS=$'\t\n'   # Split on newlines and tabs (but not on spaces)
-# }}}
+echo "Starting coverage tests..."
 
-echo "Cloning $GIT_URL"
-# Make directory
-mkdir -p $TARGET_DIR
-cd $TARGET_DIR
-
-git clone $GIT_URL .
-
-# git checkout feature/functional-tests
-
-echo "Switching to $GIT_TAG"
-git checkout tags/$GIT_TAG
+# Ensure environment is test
+source "${SCRIPT_DIR}/env.sh"
 
 npm ci
 npm run backend:start
