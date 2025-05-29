8e05ca56964baabf1515312109d879f0557da49d
Kevin Leyow <kleyow@gmail.com>
2025-05-28 21:13:56 -0500

    chore: script

--- scripts/run-functional-tests.sh
+++ scripts/run-functional-tests.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
 set -eo pipefail
 
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-PROJECT_ROOT="$(cd "${SCRIPT_DIR}/../.." && pwd)"
-
-cd "${PROJECT_ROOT}"
-
-echo "Starting coverage tests..."
-
-# Ensure environment is test
-source "${SCRIPT_DIR}/env.sh"
-
 npm ci
 npm run backend:start
 export TEST=true
