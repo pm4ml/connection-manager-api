e307275a900c4b345b4f29ac4791c8127b3b60d2
Yevhen Kyriukha <kirgene@gmail.com>
2025-11-26 21:08:33 +0200

    fix: add nvm

--- scripts/run-functional-tests.sh
+++ scripts/run-functional-tests.sh
@@ -5,12 +5,14 @@ if [ ! -f .env ]; then
   cp ./test/.env-func .env
 fi
 
+nvm use
 npm ci
 npm run backend:start
 
 cd ./test/functional-tests
 
 echo "Installing dependencies"
+nvm use
 npm i
 
 echo "Executing Functional Tests for $GIT_TAG"
