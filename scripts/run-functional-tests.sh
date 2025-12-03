68302f59c761771ad6fc5614579db90090650240
Yevhen Kyriukha <kirgene@gmail.com>
2025-12-03 10:11:46 +0200

    chore: cleanup

--- scripts/run-functional-tests.sh
+++ scripts/run-functional-tests.sh
@@ -5,14 +5,12 @@ if [ ! -f .env ]; then
   cp ./test/.env-func .env
 fi
 
-nvm use
 npm ci
 npm run backend:start
 
 cd ./test/functional-tests
 
 echo "Installing dependencies"
-nvm use
 npm i
 
 echo "Executing Functional Tests for $GIT_TAG"
