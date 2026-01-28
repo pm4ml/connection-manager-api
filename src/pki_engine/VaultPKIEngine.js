b42e4ad34a4bad6145f9da7add204cda6bdf6575
Kalin Krustev <kalin.krustev@gmail.com>
2026-01-28 10:13:37 +0000

    feat: add timestamp to certificate bundle in VaultPKIEngine

--- src/pki_engine/VaultPKIEngine.js
+++ src/pki_engine/VaultPKIEngine.js
@@ -307,6 +307,7 @@ class VaultPKIEngine extends PKIEngine {
       .sort((a, b) => b.id - a.id)[0];
     const cert = this.getCertInfo(dfspClientCert.certificate);
     const bundle = {
+      timeStamp: Date.now(),
       ca_bundle: `${dfspCA.intermediateChain}\n${dfspCA.rootCertificate}`,
       client_key: dfspClientCert.key,
       client_cert_chain: `${dfspClientCert.certificate}\n${dfspCA.intermediateChain}\n${dfspCA.rootCertificate}`,
